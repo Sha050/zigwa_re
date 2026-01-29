f7fd09efda4edf49999b943842ec751e97de4b9c
Shakthi Pranav <shakthipranav05@gmail.com>
2026-01-29 09:43:11 +0530

    first commit

---
+++ backend/zigwa_re/src/main/java/com/zigwa/zigwa_re/ZigwaReApplication.java
@@ -0,0 +1,13 @@
+package com.zigwa.zigwa_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigwaReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZigwaReApplication.class, args);
+	}
+
+}
---
+++ backend/zigwa_re/src/test/java/com/zigwa/zigwa_re/ZigwaReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zigwa.zigwa_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigwaReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
